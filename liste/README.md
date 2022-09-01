0671cf31b21383dc4f36f26824888b0a203fcba9
Andrea Borruso <aborruso@gmail.com>
2022-09-01 23:52:32 +0200

    close #3

---
+++ liste/README.md
@@ -0,0 +1,16 @@
+# Lista candidate e candidati elezioni politiche 2022
+
+Dal primo settembre 2022 (grazia a Vittorio Nicoletta per l'[avviso](https://twitter.com/vi__enne/status/1565401905622392837)) è stato pubblicato l'elenco delle liste delle candidate e dei candidati elezioni politiche 2022.
+
+Purtroppo **soltanto come pagina web**, senza rendere disponibile in modo visibile **anche l'accesso alla fonte dati in formato leggibile meccanicamente**.
+
+Li abbiamo estratti e sono disponibili in forma nativa nella cartella `[rawdata](rawdata)` (sono dei file `JSON`).
+
+Da questi sono state estratte le anagrafiche di candidate e candidati in formato `CSV`:
+
+- [CAMERA_ITALIA_20220925_pluri](./processing/CAMERA_ITALIA_20220925_pluri.csv)
+- [CAMERA_ITALIA_20220925_uni](./processing/CAMERA_ITALIA_20220925_uni.csv)
+- [CAMERA_ESTERO_20220925](./processing/CAMERA_ESTERO_20220925.csv)
+- [SENATO_ITALIA_20220925_pluri](./processing/SENATO_ITALIA_20220925_pluri.csv)
+- [SENATO_ITALIA_20220925_uni](./processing/SENATO_ITALIA_20220925_uni.csv)
+- [SENATO_ESTERO_20220925](./processing/SENATO_ESTERO_20220925.csv)
---
+++ liste/liste.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+### requisiti ###
+# Miller 5
+# jq
+# exiftool
+# pdftotext
+### requisiti ###
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+mkdir -p "$folder"/rawdata
+mkdir -p "$folder"/processing
+
+URL_base="https://dait.interno.gov.it"
+
+if [ -f "$folder"/rawdata/tmp-lista.md ]; then
+    rm "$folder"/rawdata/tmp-lista.md
+fi
+
+# scarica liste
+curl -kLs 'https://dait.interno.gov.it/elezioni/trasparenza/elezioni-politiche-2022'  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36' \
+  --compressed | sed -e '/carica/,/)/!d' | perl  -p -e 's/\n//g' | sed 's/carica/\ncarica/g' | grep -Po '/docu.+?",.+?",' | sed -r 's/ +//g;s|","|/|;s/",//' | grep -vP '.+POLI.+?/POLI.+'| while read -r line; do
+    nome=$(echo "$line" | sed -r 's|/.+/||')
+    curl -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36' "$URL_base"/"$line".json >"$folder"/rawdata/"$nome".json
+    <"$folder"/rawdata/"$nome".json jq -c '.candidati[]' | mlr --j2c unsparsify then cut -x -r -f ":" >"$folder"/processing/"$nome".csv
+    echo "[$nome](./processing/$nome.csv)" >>"$folder"/tmp-lista.md
+  done
