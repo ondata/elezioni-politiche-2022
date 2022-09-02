2d03e8347f12e3725739ea837b6671ce82c4c24b
Andrea Borruso <aborruso@gmail.com>
2022-09-02 07:49:29 +0200

    update

--- liste/liste.sh
+++ liste/liste.sh
@@ -26,6 +26,6 @@ curl -kLs 'https://dait.interno.gov.it/elezioni/trasparenza/elezioni-politiche-2
   --compressed | sed -e '/carica/,/)/!d' | perl  -p -e 's/\n//g' | sed 's/carica/\ncarica/g' | grep -Po '/docu.+?",.+?",' | sed -r 's/ +//g;s|","|/|;s/",//' | grep -vP '.+POLI.+?/POLI.+'| while read -r line; do
     nome=$(echo "$line" | sed -r 's|/.+/||')
     curl -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36' "$URL_base"/"$line".json >"$folder"/rawdata/"$nome".json
-    <"$folder"/rawdata/"$nome".json jq -c '.candidati[]' | mlr --j2c unsparsify then cut -x -r -f ":" >"$folder"/processing/"$nome".csv
+    <"$folder"/rawdata/"$nome".json jq -c '.candidati[]' | mlr --j2c unsparsify then cut -x -r -f ":" then cut -x -f cod_fisc then put '${dt_nasc}=regextract_or_else(${dt_nasc},"[0-9]{4}","")' >"$folder"/processing/"$nome".csv
     echo "[\`$nome.csv\`](./processing/$nome.csv)" >>"$folder"/tmp-lista.md
   done
