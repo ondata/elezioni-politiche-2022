ef5da093a1f6d55001b2994ab505c40b75061506
Andrea Borruso <aborruso@gmail.com>
2022-09-02 10:47:16 +0200

    update

--- liste/liste.sh
+++ liste/liste.sh
@@ -22,11 +22,16 @@ if [ -f "$folder"/tmp-lista.md ]; then
   rm "$folder"/tmp-lista.md
 fi
 
+if [ -f "$folder"/fonti_dati.txt ]; then
+  rm "$folder"/fonti_dati.txt
+fi
+
 # scarica liste
 curl -kLs 'https://dait.interno.gov.it/elezioni/trasparenza/elezioni-politiche-2022' -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36' \
   --compressed | sed -e '/carica/,/)/!d' | perl -p -e 's/\n//g' | sed 's/carica/\ncarica/g' | grep -Po '/docu.+?",.+?",' | sed -r 's/ +//g;s|","|/|;s/",//' | grep -vP '.+POLI.+?/POLI.+' | while read -r line; do
   nome=$(echo "$line" | sed -r 's|/.+/||')
-  curl -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36' "$URL_base"/"$line".json >"$folder"/rawdata/"$nome".json
+  curl -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36' "$URL_base""$line".json >"$folder"/rawdata/"$nome".json
+  echo "$URL_base""$line".json >>"$folder"/fonti_dati.txt
   jq <"$folder"/rawdata/"$nome".json -c '.candidati[]' | mlr --j2c unsparsify then cut -x -r -f ":" then put '${dt_nasc}=regextract_or_else(${dt_nasc},"[0-9]{4}","")' >"$folder"/processing/"$nome".csv
   echo "[\`$nome.csv\`](./processing/$nome.csv)" >>"$folder"/tmp-lista.md
 
