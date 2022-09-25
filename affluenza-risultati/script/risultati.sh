2f47d7c4b3903a7dac77c828d5be7c52fa042c52
Andrea Borruso <aborruso@gmail.com>
2022-09-26 00:28:40 +0200

    update

--- affluenza-risultati/script/risultati.sh
+++ affluenza-risultati/script/risultati.sh
@@ -12,6 +12,7 @@ mkdir -p "$folder"/rawdata
 mkdir -p "$folder"/processing
 
 find "$folder"/rawdata -maxdepth 1 -iname "*.json" -type f -delete
+find "$folder"/rawdata -maxdepth 1 -iname "*.csv" -type f -delete
 find "$folder"/processing -maxdepth 1 -iname "*.csv" -type f -delete
 
 ### Camera Italia ###
@@ -136,5 +137,39 @@ mlr -I --csv reorder -f CR "$folder"/processing/senato-italia-circoscrizione.csv
 mlr -I --csv cat then reshape -r ":" -o i,v then filter -S -x '$v==""' then put '$l_pos=regextract($i,"[0-9]+");$i=sub($i,".+:","")' then rename pos,r_pos then reshape -s i,v then sort -n CR,r_pos,l_pos then put -S '$perc=sub($perc,",",".")' "$folder"/processing/senato-italia-circoscrizione.csv
 
 mv "$folder"/processing/senato-italia-circoscrizione.csv "$folder"/../dati/risultati/senato-italia-circoscrizione.csv
+
+### Collegio plurinominale Camera ###
+
+mlr --c2t filter -S '$cod=~".+0000000$" && $tipo=="CP"' then put -S '$CR=sub($cod,"^([0-9]{2})([0-9]{2})([0-9]{3})([0-9]{4}).+$","\2");$CP=sub($cod,"^([0-9]{2})([0-9]{2})([0-9]{3})([0-9]{4}).+$","\3");$CU=sub($cod,"^([0-9]{2})([0-9]{2})([0-9]{3})([0-9]{4}).+$","\4")' then cut -f CR,CP,CU "$folder"/../risorse/camera_geopolitico_italia.csv | tail -n +2 >"$folder"/processing/camera_cp.tsv
+
+while IFS=$'\t' read -r CR CP CU; do
+
+  curl "https://eleapi.interno.gov.it/siel/PX/scrutiniCI/DE/20220925/TE/02/CR/$CR/CP/$CP" \
+    -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0' \
+    -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+    -H 'Accept-Language: it,en-US;q=0.7,en;q=0.3' \
+    -H 'Accept-Encoding: gzip, deflate, br' \
+    -H 'Content-Type: application/json' \
+    -H 'Origin: https://elezioni.interno.gov.it' \
+    -H 'DNT: 1' \
+    -H 'Connection: keep-alive' \
+    -H 'Referer: https://elezioni.interno.gov.it/' \
+    -H 'Sec-Fetch-Dest: empty' \
+    -H 'Sec-Fetch-Mode: cors' \
+    -H 'Sec-Fetch-Site: same-site' \
+    -H 'Pragma: no-cache' \
+    -H 'Cache-Control: no-cache' \
+    -H 'TE: trailers' --compressed |
+    jq . >"$folder"/rawdata/camera-italia-plurinominale_"$CR-$CP".json
+  jq <"$folder"/rawdata/camera-italia-plurinominale_"$CR-$CP".json '.cand[] | . |= .+ {CR_CP:"'"$CR-$CP"'"}'  | mlr --j2c unsparsify >"$folder"/processing/camera-italia-plurinominale_"$CR-$CP".csv
+
+done <"$folder"/processing/camera_cp.tsv
+
+mlr --csv unsparsify "$folder"/processing/camera-italia-plurinominale_*.csv >"$folder"/processing/camera-italia-plurinominale.csv
+mlr -I --csv reorder -f CR_CP "$folder"/processing/camera-italia-plurinominale.csv
+
+mlr -I --csv cat then reshape -r ":" -o i,v then filter -S -x '$v==""' then put -S '$l_pos=regextract($i,"[0-9]+");$i=sub($i,".+:","")' then rename pos,r_pos then reshape -s i,v then sort -f CR_CP -n r_pos,l_pos then put -S '$perc=sub($perc,",",".")' "$folder"/processing/camera-italia-plurinominale.csv
+
+mv "$folder"/processing/camera-italia-plurinominale.csv "$folder"/../dati/risultati/camera-italia-plurinominale.csv
 
 exit 0
